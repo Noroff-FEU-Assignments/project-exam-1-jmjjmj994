e82c8101b939d0d35d99eb6e044ba452056192d3
jmjjmj994 <jonathan.mjos@outlook.com>
2023-04-29 21:30:06 +0200

    Did some changes in JS, so instead of using clientWidth which was used BEFORE the slider went 600% wide, i declared that slideWidth = 880. So now the slider shows two and two cards on each click. Some minor adjustments need to be made when the screen is below 1200px,

--- js/fetch.js
+++ js/fetch.js
@@ -1,39 +1,55 @@
-import { slide, slideOne, slideTwo } from "./global.js";
+import { slide } from "./global.js";
 
-/* const url = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts?_embed=wp:featuredmedia"
+const url = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts?_embed=wp:featuredmedia";
+const category = "https://exam1.braatenmjos.no/wp-json/wp/v2/categories";
 
 
 
+
+fetch(category)
+.then((res => res.json()))
+.then((data)=>{
+ ts(data)
+  /* sliderOne(data)  */  
+ 
+})
 
 fetch(url)
 .then((res => res.json()))
 .then((data)=>{
-  sliderOne(data)   
- sliderTwo(data)
+ 
+  sliderOne(data)  
+ 
 })
 
 
+function ts(data) {
+ data.filter((ele, index)=>{
+
+  const {slug,name} = ele
+if(name === "Composer"){
+  console.log(name)
+}
+ });
+}
 
 
 
 
-
-function sliderOne(data){
-slideOne.innerHTML = data.map((artist,index) =>{
+ function sliderOne(data){
+slide.innerHTML = data.map((artist,index) =>{
  
     const  {id, date, type, title,guid}  = artist;  
-    let media = artist._embedded["wp:featuredmedia"][0].source_url
+    if(index > 7){
+      return
+    }
+   let media = artist._embedded["wp:featuredmedia"][0].source_url  
   
-  if(index > 3) {
-    return
-  } 
+ 
   
 
 return `
-<div class="card" style="background-image:url(${media})">
-
-   
-
+<div class="slide-card" style="background-image:url(${media})">
 <div class="card-text">
   <p class="blog-card-date">${date}</p>
   <h2 class="blog-card-title">${title.rendered}</h2>
@@ -45,32 +61,5 @@ return `
 `
 }).join("");
 };  
+ 
 
-
-function sliderTwo(data){
-    slideTwo.innerHTML = data.map((artist,index) =>{
-     
-        const  {id, date, type, title,guid}  = artist;  
-        let media = artist._embedded["wp:featuredmedia"][0].source_url
-       console.log(media);
-      if(index < 6) {
-        return
-      }
-      
-    
-    return `
-    <div class="card">
-    <div class="card-img">
-       <img src="${media}"/>
-    </div>
-    <div class="card-text">
-      <p class="blog-card-date">${date}</p>
-      <h2 class="blog-card-title">${title.rendered}</h2>
-    </div>
-    
-    </div>
-    
-    
-    `
-    }).join("");
-    };   */
--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 "use strict";
 /*Hamburger*/
-export const slideOne = document.querySelector(".slide-one");
-export const slideTwo = document.querySelector(".slide-two");
+export const slide = document.querySelector(".slide");
+
 const hamburgerMenu = document.querySelector(".hamburger");
 const dropdownMenu = document.querySelector(".dropdown-menu")
 
@@ -18,14 +18,15 @@ if(!menuOpen) {
 }
 
 };
-/*Hamburger*/
+
 
 
 const sliderContainer = document.querySelector(".slider-container");
-export const slide = document.querySelector(".slide");
+
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
-const slideWidth = slide.clientWidth;
+/* const slideWidth = slide.clientWidth; */
+const slideWidth = 880;
 console.log(slideWidth);
 
 nextBtn.onclick = (e) => {
@@ -36,48 +37,3 @@ prevBtn.onclick = (e) => {
     sliderContainer.scrollLeft -= slideWidth
     };
 
-/* window.onclick = (e) => {
-    if (e.target !== main){
-        dropdownMenu.classList.toggle("active")
-      }
-}
- */
-
-
-
-
-    
-  
-/* const posts = [
-{name:"Austin Wintory", date:"2023-04-26T08:04:29", img:"exam-img/Austin_Wintory_with_LSO.jpg", role:"composer"},
-{name:"Nat King Cole", date:"2023-04-26T08:04:29", img:"exam-img/1091911_nat_king_cole_sepia_portrait.jpg", role:"artist"},
-{name:"Frank Sinatra", date:"2023-04-26T08:04:29", img:"exam-img/frank-sinatra-1945-willinger-portrait-c8e0bd-1024.jpg", role:"artist"},
-{name:"Giacomo Puccini", date:"2023-04-26T08:04:29", img:"exam-img/giacomo-puccini-640.jpg", role:"composer"}
-]
-
-
-
-const carouselContainer = document.querySelector(".carousel-container");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-
-carouselContainer.innerHTML = posts.map((person, slideIndex) =>{
-const{name, date, img, role} = person;
-let position = "next";
-if(slideIndex === 0){
-    position="active";
-}
-if(slideIndex === person.length - 1){
-    position === "last"
-}
-return ` <article class="slide ${position} ">
-<img src="${img}"/>
-<h4>${name}
-</h4>
-<p class="title">${role}</p>
-<p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa maiores mollitia atque odit cum? Ad repudiandae optio ea labore harum beatae, neque rem facilis doloremque. Cum eaque atque impedit dolorum?</p>
-<p class="date"> ${date}</p>
-</article>`
-}).join(""); */
- 
-//slides
