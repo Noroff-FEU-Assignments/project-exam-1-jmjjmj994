ecc259d125e4827b3a889fd4a161af90ed7abadf
jmjjmj994 <jonathan.mjos@outlook.com>
2023-05-02 12:29:32 +0200

    added slider functionality. As last time i decided to go with ONE slide, containing 8 cards. And as last time instead of clientWidth i decided to go for a 800px onclick

---
+++ js/global.js
@@ -0,0 +1,13 @@
+const slideContainer = document.querySelector(".slide-container");
+const slide = document.querySelector(".slide");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+const slideWidth = 880;
+
+nextBtn.onclick = (e) => {
+slideContainer.scrollLeft +=slideWidth
+};
+prevBtn.onclick = (e) => {
+    slideContainer.scrollLeft -=slideWidth
+    };
