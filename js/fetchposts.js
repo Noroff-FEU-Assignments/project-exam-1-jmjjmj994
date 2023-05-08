8a1c45a12f32664bfc5307bfdb94edd7efb7f86e
jmjjmj994 <jonathan.mjos@outlook.com>
2023-05-08 13:19:12 +0200

    added images to post section. Just a little bit of adjusting now, and all is done. Now over to styling, to make sure things look good

--- js/fetchposts.js
+++ js/fetchposts.js
@@ -1,6 +1,6 @@
 
-const urlPageOne = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts/?page=1";
-const urlPageTwo = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts/?page=2";
+const urlPageOne = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=1";
+const urlPageTwo = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&page=2";
 const pageOneSection = document.querySelector(".posts-page--one");
 const pageTwoSection = document.querySelector(".posts-page--two");
 
@@ -27,13 +27,13 @@ console.log(data._embedded)
 pageOneSection.innerHTML = data.map((rowOne, index) => {
    
 const {title,excerpt,id } = rowOne;
-
+const media = rowOne._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url
 return `
 
 <div class="posts-card">
 
 <div class="posts-card--title"><h1>${title.rendered}</h1></div>
-<div class="posts-card--img"> </div>
+<div class="posts-card--img"><img src="${media}"> </div>
 <div class="posts-card--content"><p>${excerpt.rendered}</p></div>
 <div class="posts-card--footer">
     <a href="/specific.html?id=${id}"></a>
@@ -48,15 +48,17 @@ return `
 
 
 function pageTwo(data){
-    pageTwoSection.innerHTML = data.map((rowOne, index) => {
+    pageTwoSection.innerHTML = data.map((rowTwo, index) => {
       
-    const {title,excerpt, id } = rowOne;
+    const {title,excerpt, id } = rowTwo;
+    const media = rowTwo._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url
     
     return `
     
     <div class="posts-card">
     
     <div class="posts-card--title"><h1>${title.rendered}</h1></div>
+    <div class="posts-card--img"><img src="${media}"> </div>
     <div class="posts-card--content"><p>${excerpt.rendered}</p></div>
     <div class="posts-card--footer">
     <a href="/specific.html?id=${id}"></a>
