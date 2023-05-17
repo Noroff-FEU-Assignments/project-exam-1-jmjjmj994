43460fee4f360705ba2e306adef92359b95f4ceb
jmjjmj994 <jonathan.mjos@outlook.com>
2023-05-17 15:52:21 +0200

    #

--- js/fetchposts.js
+++ js/fetchposts.js
@@ -21,7 +21,7 @@ const dateOldestBtn = document.querySelector("[data-date-oldest]");
 
 
 
-getData()
+const invokeFetch = getData()
 function getData(){
 fetch(url)
 .then((response) => {
@@ -31,14 +31,81 @@ fetch(url)
 .then((data) =>{
 const storeData = data;
 loader.remove()
-destructData(storeData)
+/* destructData(storeData) */
 /* sortedItems(storeData) */
+handleData(storeData);
 
 });};
 
+
+
+function handleData(data){
+   
+const myData = data;
+
+(function(){
+const firstDeck = myData.slice(0, 10);
+firstDeck.map(({title, id, _embedded, date}) => {
+    postCount.textContent = 10
+    postTotal.textContent = 20
+    const image = _embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+    const alt = _embedded["wp:featuredmedia"][0].alt_text;
+    const cardDate = new Date(date);
+    const dateOptions = { year: "numeric", month: "long", day: "numeric" };
+    const norwegianDate = cardDate.toLocaleDateString("nb-NO", dateOptions);
+    cardContainer.innerHTML += `
+      <div class="posts-card">
+        <div class="posts-card--image" data-image>
+          <img src="${image}" alt="${alt}">
+        </div>
+        <div class="posts-card--title" data-title>    
+          <h1>${title.rendered} </h1>
+        </div>
+        <div class="posts-card--content" data-content>
+          <p>${norwegianDate}</p>
+        </div>
+        <div class="posts-card--link" data-link>
+          <a href="/specific.html?id=${id}" class="posts-card-link">Read Post</a>
+        </div>
+      </div>`;
+
+})}()) 
+function secondDeck(){
+const secondDeck = myData.slice(10, 20) 
+secondDeck.map(({title, id, _embedded, date}) => {
+    postCount.textContent = 20
+    postTotal.textContent = 20
+    const image = _embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+    const alt = _embedded["wp:featuredmedia"][0].alt_text;
+    const cardDate = new Date(date);
+    const dateOptions = { year: "numeric", month: "long", day: "numeric" };
+    const norwegianDate = cardDate.toLocaleDateString("nb-NO", dateOptions);
+    cardContainer.innerHTML += `
+      <div class="posts-card">
+        <div class="posts-card--image" data-image>
+          <img src="${image}" alt="${alt}">
+        </div>
+        <div class="posts-card--title" data-title>    
+          <h1>${title.rendered} </h1>
+        </div>
+        <div class="posts-card--content" data-content>
+          <p>${norwegianDate}</p>
+        </div>
+        <div class="posts-card--link" data-link>
+          <a href="/specific.html?id=${id}" class="posts-card-link">Read Post</a>
+        </div>
+      </div>`;
+      loadMoreButton.remove()
+});
+}
+loadMoreButton.addEventListener("click", secondDeck)}; 
+
+
+
 /*Function for cards*/
-function destructData(data){
-     
+
+
+/* function destructData(data){
     const firstDeck = data.slice(0, 10);
     firstDeck.map(({title, id, _embedded,date}) => firstDeckCards(title, id, _embedded, date))
     const secondDeck = data.slice(10, 20)
@@ -67,7 +134,7 @@ function firstDeckCards(header, id, media, date) {
       </div>`;
   }
 
-function secondDeckCards(){ /*change this its in the scope*/
+function secondDeckCards(){ 
     postCount.textContent =20
     postTotal.textContent = 20
 secondDeck.map(({title, id, _embedded,date}) => {
@@ -100,7 +167,7 @@ const loadMoreButton = document.querySelector(".load-more");
 loadMoreButton.addEventListener("click", secondDeckCards)
 
 };
-
+ */
 
 
 
