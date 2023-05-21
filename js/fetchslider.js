2579591236317cb3f5420d4145d42a956c4ec8be
jmjjmj994 <jonathan.mjos@outlook.com>
2023-05-21 21:25:31 +0200

    #

--- js/fetchslider.js
+++ js/fetchslider.js
@@ -1,6 +1,7 @@
 import { slide } from "./slider.js";
 
 const url = "https://exam1.braatenmjos.no/wp-json/wp/v2/posts?_embed=wp:featuredmedia";
+const urlTwo ="https://exam1.braatenmjos.no/wp-json/wp/v2/posts/?_embed=wp:featuredmedia&per_page=20";
 
 
 
@@ -15,7 +16,7 @@ fetch(url)
  const storeData = data;
 
  destructureData(storeData);
-
+ 
 
 
 })
@@ -44,4 +45,58 @@ slide.innerHTML += `
 }
 
 
+
+const mainLeftContainer = document.querySelector(".main-container-left--column");
+const mainRightContainer = document.querySelector(".main-container-right--column");
+/*47 */
+
+fetch(urlTwo)
+.then(response => response.json())
+.then((data) => {
+
+
+renderLargePost(data)
+});
+
+
+function renderLargePost(data){
+  
+  
+const myData = data;
+myData.filter(({title, id, _embedded, date})=>{
+  const image = _embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+  const alt = _embedded["wp:featuredmedia"][0].alt_text;
+  const cardDate = new Date(date);
+  const dateOptions = { year: "numeric", month: "long", day: "numeric" };
+  const norwegianDate = cardDate.toLocaleDateString("nb-NO", dateOptions);
+if(id === 47){
+mainLeftContainer.innerHTML = `
+
+<img src="${image}" alt="${alt}">
+
+`
+
+mainRightContainer.innerHTML = `
+<div class="main-right-content">
+<p>${norwegianDate} </p>
+
+<h1> ${title.rendered}</h1>
+
+<div class="main-right-footer">
+<a href="/specific.html?id=${id}">Read Post </a>
+</div>
+
+
+
+
+</div>
+
+
+
+
+`
+
+};
+})
+}
 
