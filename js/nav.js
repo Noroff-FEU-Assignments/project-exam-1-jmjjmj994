32b67cb45f958607745e73a774bbdc9513c40fd6
jmjjmj994 <jonathan.mjos@outlook.com>
2023-05-28 14:35:24 +0200

    #

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const navUl = document.querySelector(".nav-ul");
+const navItems = document.querySelectorAll("nav a");
+
 let menuOpen = false;
 hamburger.onclick = () => {
   if (!menuOpen) {
@@ -15,3 +17,11 @@ hamburger.onclick = () => {
     menuOpen = false;
   }
 };
+
+const activePage = window.location.pathname;
+
+navItems.forEach((item) => {
+  if (item.href.includes(`${activePage}`)) {
+    item.style.cssText = "text-decoration: underline; ";
+  }
+});
